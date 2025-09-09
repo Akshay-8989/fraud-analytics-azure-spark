8cc3f4d86562be2bdbfea063880338d411d7e002
Akshay-8989 <143429802+Akshay-8989@users.noreply.github.com>
2025-09-09 09:12:47 +0530

    Update config.py

--- fraud_detection/config.py
+++ fraud_detection/config.py
@@ -12,4 +12,20 @@ AZURE_BLOB_URL = f"wasbs://{CONTAINER_NAME}@{AZURE_STORAGE_ACCOUNT_NAME}.blob.co
 SPARK_JARS_PATH = "jars-folder-path"   # example - "C:/spark/spark-3.5.4-bin-hadoop3/jars"
 
 # Define common feature columns for reuse
-FEATURE_COLS = ["amount", "transaction_type_indexed", "account_age", "num_prev_transactions"]
+
+# config.py
+
+FEATURE_COLS = [
+    "transaction_type_indexed",
+    "amount",
+    "hour_of_day",
+    "day_of_week",
+    "geo_location_mismatch",
+    "device_id_indexed",
+    "balance_diff_orig",
+    "balance_diff_dest",
+    "past_transactions_count",
+    "is_large_transaction"
+]
+
+LABEL_COL = "isFraud"
